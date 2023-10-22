0ba9855d0a1751316c82f9534d71dd18b6293790
hojun <hongpc0099@gmail.com>
2023-10-22 19:16:40 +0900

    add code

---
+++ 이상한 문자 만들기.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String solution(String s) {
+        String answer = "";
+        String[] sArr = s.split("");
+        int idx = 0;
+        
+        for (int i=0; i<sArr.length; i++) {
+            if(sArr[i].equals(" ")) {
+                idx = 1;
+            }
+            answer += idx%2 == 0 ? sArr[i].toUpperCase() : sArr[i].toLowerCase() ; 
+            idx++;
+        }
+         return answer;
+    }
+}
