d66126a700679057a172f344f0236a968e03489f
hojunlee <hongpc0099@gmail.com>
2023-11-14 15:32:37 +0900

    add level2 code

---
+++ programmers/level 2/짝지어 제거하기.java
@@ -0,0 +1,18 @@
+import java.util.Stack;
+
+class Solution
+{
+    public int solution(String s)
+    {
+        int answer = 0;
+        Stack<Character> stack = new Stack<>();
+
+        for(char c : s.toCharArray())
+            if(!stack.isEmpty() && stack.peek() == c) stack.pop();
+            else stack.push(c);
+        
+        answer = stack.isEmpty() ? 1 : 0;
+
+        return answer;
+    }
+}
