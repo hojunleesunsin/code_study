8ff1ab852dd129bc9d7a52351dc9fef83ce03f40
hojunlee <hongpc0099@gmail.com>
2024-01-22 14:40:50 +0900

    update level1 code 치킨쿠폰
    level1 치킨 쿠폰 code 필요 없는 부분 삭제

--- programmers/level 0,1/치킨쿠폰.java
+++ programmers/level 0,1/치킨쿠폰.java
@@ -12,23 +12,3 @@ class Solution {
         return answer;
     }
 }
-
-new = 108
-rest = 1
-chicken = 109
-answer = 108
-
-new = 10
-rest = 9
-chicken = 19
-answer = 118
-
-new = 1
-rest = 9
-chicken = 10  
-answer = 119
-
-new = 1
-rest = 0
-chicken = 1
-answer = 120
