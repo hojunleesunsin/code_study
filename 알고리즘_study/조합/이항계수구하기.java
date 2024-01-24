786fb77748d6923b0709c35da96eace8db528f40
hojunlee <hongpc0099@gmail.com>
2024-01-24 14:26:15 +0900

    add 조합 관련 문제
    조합 관련 문제 이항 계수 구하기 추가
    조합_정리와 이항계수구하기 문제 모아둠.

---
+++ 알고리즘_study/조합/이항계수구하기.java
@@ -0,0 +1,33 @@
+// 조합 관련 문제 => 이항 계수 구하기
+
+// 입력 예시
+// 6 3 
+
+package 알고리즘_study.조합;
+
+import java.util.Scanner;
+
+public class 이항계수구하기 {
+    static int N, K;
+    static int D[][];
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+        K = sc.nextInt();
+        D = new int[N+1][N+1];
+        // 초기화
+        for(int i = 0; i <= N; i++) {
+            D[i][i] = 1;
+            D[i][0] = 1;
+            D[i][1] = i;
+        }
+
+        // 점화식으로 배열 완성하기
+        for(int i = 2; i <=N; i++) {
+            for(int j = 1; j < i; j ++) {
+                D[i][j] = D[i-1][j] + D[i-1][j-1];
+            }
+        }
+        System.out.println(D[N][K]);
+    }
+}
